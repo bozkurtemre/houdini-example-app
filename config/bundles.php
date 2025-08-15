ddfa89e0de4fb92d2eac06c5b8f902e208823f8c
Emre Bozkurt <emre75kurt@gmail.com>
2025-08-15 15:54:06 +0300

    first commit

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Houdini\HoudiniBundle\HoudiniBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+];
